3703b40c68d030948e36e8370a02909f7508ddc5
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-05-05 19:44:47 +0700

    Add css Lib

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import thunk from "redux-thunk";
+import App from "./App";
+import { applyMiddleware, compose, createStore } from "redux";
+import { rootReducer } from "./Redux/reducer";
+import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+let store = createStore(rootReducer, composeEnhancers(applyMiddleware(thunk)));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
+  <Provider store={store}>
     <App />
-  </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
