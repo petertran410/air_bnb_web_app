a8c810c558d1d4ad84edf4c1461975004fc93cbb
Nguyen Huynh Anh Tu <91893958+nhatuwork@users.noreply.github.com>
2024-05-13 16:07:36 +0700

    Done LoginSign---TuDev

--- src/LoginSign/LoginSign.js
+++ src/LoginSign/LoginSign.js
@@ -58,4 +58,225 @@ import {
           console.log(err);
         });
     };
-  
+    const renderLogin = () => {
+        return (
+          <Form
+            className="w-full"
+            layout="vertical"
+            name="basic"
+            labelCol={{
+              span: 24,
+            }}
+            wrapperCol={{
+              span: 24,
+            }}
+            initialValues={{
+              remember: true,
+            }}
+            onFinish={onFinishLogin}
+            autoComplete="off"
+          >
+            <Form.Item
+              label="Username"
+              name="email"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your username!",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+    
+            <Form.Item
+              label="Password"
+              name="password"
+              rules={[
+                {
+                  required: true,
+                  message: "Please input your password!",
+                },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item
+              name="remember"
+              valuePropName="checked"
+              wrapperCol={{
+                offset: 8,
+                span: 16,
+              }}
+            >
+              <Button type="primary" htmlType="submit">
+                Submit
+              </Button>
+            </Form.Item>
+          </Form>
+        );
+      };
+      const renderSign = () => {
+        return (
+          <Form
+            {...formItemLayout}
+            form={form}
+            name="register"
+            onFinish={onFinishSign}
+            scrollToFirstError
+          >
+            <Form.Item
+              name="email"
+              label="E-mail"
+              rules={[
+                {
+                  type: "email",
+                  message: "Không hợp lệ",
+                },
+                {
+                  required: true,
+                  message: "Xin nhập Email",
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+    
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[
+                {
+                  required: true,
+                  message: "Hãy nhập pass",
+                },
+              ]}
+              hasFeedback
+            >
+              <Input.Password />
+            </Form.Item>
+    
+            <Form.Item
+              name="confirm"
+              label="Confirm"
+              dependencies={["password"]}
+              hasFeedback
+              rules={[
+                {
+                  required: true,
+                  message: "Xin nhập lại pass",
+                },
+                ({ getFieldValue }) => ({
+                  validator(_, value) {
+                    if (!value || getFieldValue("password") === value) {
+                      return Promise.resolve();
+                    }
+    
+                    return Promise.reject(new Error("Pass nhập lại không giống"));
+                  },
+                }),
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+            <Form.Item
+              name="birthday"
+              label="Ngày sinh"
+              rules={[
+                {
+                  required: true,
+                  message: "Không bỏ trống",
+                  whitespace: true,
+                },
+              ]}
+            >
+              <Input type={"date"} />
+            </Form.Item>
+            <Form.Item
+              rules={[
+                {
+                  required: true,
+                  message: "Không bỏ trống",
+                },
+              ]}
+              name="gender"
+              label="Giới tính"
+            >
+              <Select
+                style={{
+                  width: 120,
+                }}
+                options={[
+                  {
+                    value: "nam",
+                    label: "Nam",
+                  },
+                  {
+                    value: "nu",
+                    label: "Nữ",
+                  },
+                ]}
+              />
+            </Form.Item>
+            <Form.Item
+              name="name"
+              label="Full name"
+              rules={[
+                {
+                  required: true,
+                  message: "Không bỏ trống",
+                  whitespace: true,
+                },
+              ]}
+            >
+              <Input />
+            </Form.Item>
+    
+            <Form.Item
+              name="phone"
+              label="Phone"
+              rules={[
+                {
+                  required: true,
+                  message: "Không bỏ trống",
+                },
+              ]}
+            >
+              <Input
+                type={"number"}
+                style={{
+                  width: "100%",
+                }}
+              />
+            </Form.Item>
+    
+            <Form.Item
+              name="agreement"
+              valuePropName="checked"
+              rules={[
+                {
+                  validator: (_, value) =>
+                    value
+                      ? Promise.resolve()
+                      : Promise.reject(new Error("Xin chấp nhận điều khoản")),
+                },
+              ]}
+              {...tailFormItemLayout}
+            >
+              <Checkbox>
+                Tôi đã đọc và <a href="">chấp nhận các điều khoản</a>
+              </Checkbox>
+            </Form.Item>
+            <Form.Item {...tailFormItemLayout}>
+              <Button type="primary" htmlType="submit">
+                Đăng Kí
+              </Button>
+            </Form.Item>
+          </Form>
+        );
+      };
+      const [form] = Form.useForm();
+      return <>{isLogin ? renderLogin() : renderSign()}</>;
+    };
+    
+    export default LoginSign;
