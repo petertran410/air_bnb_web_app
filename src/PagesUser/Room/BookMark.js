8d3273a2f62567f0149f47717385ab613ed3b2dd
Ngoc Nhan <60886911+petertran410@users.noreply.github.com>
2024-05-13 16:25:36 +0700

    Add Comment Box and Book Marker

---
+++ src/PagesUser/Room/BookMark.js
@@ -0,0 +1,38 @@
+import React, { useState, useEffect } from "react";
+import { message } from "antd";
+import { localLike, localServ } from "../../Services/localService";
+import { HeartIcon } from "../../Utilities/IconSVG";
+import { CiHeart } from "react-icons/ci";
+export default function BookMark({ id }) {
+  let dataLocal = localLike.like.get();
+  let userInfo = localServ.user.get();
+  const [isBookMark, setIsBookMark] = useState(false);
+  useEffect(() => {
+    dataLocal.includes(id) && setIsBookMark(true);
+  }, []);
+  const changeIcon = () => {
+    if (!userInfo) {
+      message.error("Bạn cần đăng nhập để thực hiện chức năng này");
+      return;
+    } else {
+      setIsBookMark(!isBookMark);
+      let i = dataLocal.findIndex((item) => item === id);
+      i === -1 ? dataLocal.push(id) : dataLocal.splice(i, 1);
+      localLike.like.set(dataLocal, "key");
+    }
+  };
+  return (
+    <>
+      {isBookMark ? (
+        <HeartIcon changeIcon={changeIcon} />
+      ) : (
+        <CiHeart
+          className="w-6 h-6"
+          onClick={() => {
+            changeIcon();
+          }}
+        />
+      )}
+    </>
+  );
+}
